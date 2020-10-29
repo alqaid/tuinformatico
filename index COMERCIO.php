186e9b1523c413c21f8cb13a8dc5792059298c3c
alqaid <alqaid@gmail.com>
2020-10-29 17:03:50 +0100

    version 2
    con modal de registro

---
+++ index COMERCIO.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TU INFORMATICO</title>
+  <meta charset="utf-8">
+   
+</head>
+<body>
+<p>HOLA MUNDO</p>
+<?php 
+	echo " hola Hernan";
+?>
+</body>
+</html>
--- sql/vistaContratacionesInformaticosEmpresas.sql
+++
@@ -1,4 +0,0 @@
-SELECT empresas.eNombre AS Empresa,informaticos.iNombre as Informatico ,contratos.cPuntuacion as Puntuacion, contratos.cFechaInicio,contratos.cFechaFin
-FROM `contratos`,`empresas`,`informaticos` WHERE 
-contratos.ceClaveEmpresas=empresas.eClave and contratos.ciClaveInformaticos=informaticos.iClave
-ORDER BY empresas.eNombre,informaticos.iNombre,contratos.cPuntuacion
