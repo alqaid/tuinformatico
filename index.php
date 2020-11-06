faf88c31ac6a612ad9c05202aaad0a494762240b
Damian1789 <36619122+Damian1789@users.noreply.github.com>
2020-11-06 18:32:13 +0100

    Un cambio pequeño en el Registro del index

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>TU INFORMÁTICO - NUEVO CONTRATO</title>
+<?php
+	require('head.php');
+?>
+</head>
+<body>
+<?php
+	require('cabecera.php');
+?>
+<main class=" col-9 "   >
+<!-- SITIO LIBRE PARA INCLUIR -->
+
+
+                    <div class = "row">
+                        <div class="card col "   >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title ">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+                        <div class="card col "   >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col "  >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col " >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                    </div> 
+
+
+
+                    <div class = "row">
+
+                        <div class="card col "  >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col "  >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col " >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                    </div> 
+
+
+
+
+
+                    <div class = "row">
+
+                        <div class="card col "  >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col "  >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                        <div class="card col " >
+                            <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a onclick="alert('Es necesario autentificarse para poder acceder a esa información, si no tienes cuenta hazte pulsando en Registrar')" class="btn btn-success">See Profile</a>
+                            </div>
+                        </div>
+
+                    </div> 
+
+
+<!-- FIN SITIO LIBRE PARA INCLUIR -->
+</main> <!-- class="container" -->
+	<aside class=" col-3 "   > <!-- BANNER -->
+		<h2>Compartenos en tus redes sociales<h2>
+		<a href="https://twitter.com/?lang=es">
+		<img   src="images/twitter.png" alt=""  width="100%" height="520px">
+		</a>
+	</aside> <!-- FIN BANNER -->
+ <?php
+	require('pie.php');
+?>
+                    <!-- The Modal -->
+                    <DIV ALIGN=center>
+                        <div class="modal fade" id="identificacion">
+
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Identificarse</h4>
+                                        <button type="button" class="close" data-dismiss="modal">×</button>
+                                    </div>
+
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                        <form method="post"
+                                            <label for="e">Email:</label><br>
+                                            <input type="text" id="e" name="email"><br>
+                                            <label for="p">Contraseña:</label><br>
+                                            <input type="password" id="p" name="contraseña"><br>
+                                            <a href="https://www.w3schools.com">¿Olvido su contraseña?</a><br><br>
+                                            <label><input type="checkbox"> Recordadme</label><br>
+                                            <button type="submit" class="btn btn-success">Iniciar sesion</button>
+                                        </form>
+                                        <a href="" data-dismiss="modal" data-toggle="modal" data-target="#registro">¡Registrate!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </DIV>
+					
+					<!-- The Modal -->
+                    <DIV ALIGN=center>
+                        <div class="modal fade" id="registro">
+
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">¿Usted es?</h4>
+                                        <button type="button" class="close" data-dismiss="modal">×</button>
+                                    </div>
+
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+
+										<form action="registrarEmpresa.php">
+											<button type="submit" class="btn btn-success">Empresa</button>
+										</form>	<br>
+										<label>o</label><br>
+										<form action="RegistroIformatico.html">
+											<button type="submit" class="btn btn-success">Informatico</button>
+										</form>												
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </DIV>
+                            </body>
+                            </html>
