d94a50a6021c5ecde10435a2aaf1f84d2be544ce
joestheorigen <56586353+joestheorigen@users.noreply.github.com>
2020-11-13 13:52:37 +0100

    volvieron

---
+++ servicios/config.php
@@ -0,0 +1,12 @@
+<?php
+define('USER', 'root');
+define('PASSWORD', '');
+define('HOST', 'localhost');
+define('DATABASE', 'tuinformatico');
+ 
+try {
+    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
+}
+?>
