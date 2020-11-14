5a687a805bcb8d6470bf0d420e3a4748bac24e7b
joestheorigen <56586353+joestheorigen@users.noreply.github.com>
2020-11-14 17:40:28 +0100

    no se si funcionara

--- cabecera.php
+++ cabecera.php
@@ -1,3 +1,21 @@
+   <?php
+session_start();
+ 
+if(!isset($_SESSION['eClave'])){
+	
+	echo "<script>";
+	echo "empresa();";
+	echo "</script>";
+} else if(!isset($_SESSION['iClave'])){
+	
+	echo "<script>";
+	echo "informatico();";
+	echo "</script>";
+}else{
+	// Show users the page!
+}
+?>
+
    <!-- fila  superior  herramientas -->
         <nav class="navbar navbar-expand-sm  navbar-dark bg-dark justify-content-end">
             <a 
@@ -17,11 +35,16 @@
                     </form>
 
                     <img src="https://www.vayagif.com/images/banano.gif" alt="Mascota">
-
-                    <a type="button" class="navbar-brand" data-toggle="modal" data-target="#registro"> Registrarse </a>
+					
+					<svg id="nombre" style="display:none" height="50" width="200">
+                    <text fill="#28a745" font-size="20" font-family="Verdana" x="10" y="40">Tuinformatico.com</text>
+                    Error de logo.
+					</a>
+                    </svg>
+					
+					<a type="button" class="navbar-brand" data-toggle="modal" data-target="#registro"> Registrarse </a>
 					
                     <a type="button" class="navbar-brand" data-toggle="modal" data-target="#identificacion"> Identificarse </a>
-				      </div>
             </a>
         </nav>
 
@@ -36,11 +59,11 @@
 	<!-- ojo: EN CADA PAGINA LLAMAMOS 
 		document.getElementById("menu_cabecera_bus_oft").className  = "nuestromenus_activado";
 		PARA CAMBIAR EN ENLACE ACTIVO-->
-		  <li><a id="menu_cabecera_home"   href="index.php">Home</a></li>
-		  <li><a id="menu_cabecera_reg_inf" href="RegistroInformatico.php">Nuevo Profesional</a></li>
-		  <li><a id="menu_cabecera_reg_emp" href="registrarEmpresa.php">Nueva Empresa</a></li>
-		  <li><a id="menu_cabecera_reg_con" href="nuevocontrato.php">Nueva Oferta</a></li>
+		  <li><a id="menu_cabecera_home" href="index.php">Informaticos</a></li>
 		  <li><a id="menu_cabecera_bus_oft" href="buscarofertas.php">Ofertas</a></li>
+		  <li><a id="menu_cabecera_reg_con" style="display:none" href="nuevocontrato.php">Nueva Oferta</a></li>
+		  <li><a id="menu_cabecera_datos" style="display:none" href="buscarofertas.php">Cambiar datos</a></li>
+		  <li><a id="menu_cabecera_cerrar_Session" style="display:none" href="buscarofertas.php">Cerrar sesion</a></li>
 	</ul> 
 </nav>
 
@@ -51,4 +74,22 @@
 		    <div class="container">
             <div class = "row">
 
-                
+        <script>
+		function informatico(){  
+		document.getElementById("menu_cabecera_datos").style = "display:inline";
+		document.getElementById("menu_cabecera_cerrar_Session").style = "display:inline";
+		document.getElementById("menu_cabecera_reg_con").style = "display:none";
+		document.getElementById("menu_cabecera_datos").href = "index.php";
+		document.getElementById("nombre").style = "display:inline";
+		}
+		</script>
+		
+		<script>
+		function empresa(){  
+		document.getElementById("menu_cabecera_datos").style = "display:inline";
+		document.getElementById("menu_cabecera_cerrar_Session").style = "display:inline";
+		document.getElementById("menu_cabecera_reg_con").style = "display:inline";
+		document.getElementById("menu_cabecera_datos").href = "index.php";
+		document.getElementById("nombre").style = "display:inline";
+		}
+		</script>
