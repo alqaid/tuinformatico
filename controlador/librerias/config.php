713d969cb72f0734dabe3d9b0616bdfe69943ba8
Angel <alqaid@gmail.com>
2020-11-24 20:40:31 +0100

    reescribir config

--- controlador/librerias/config.php
+++ controlador/librerias/config.php
@@ -9,8 +9,8 @@ $mail->SMTPAuth      = true;                  // enable SMTP authentication
 //$mail->SMTPKeepAlive = true;                  // SMTP connection will not close after each email sent
 $mail->Host          = "smtp.ionos.es"; // sets the SMTP server
 $mail->Port          = 587;                    // set the SMTP port for the GMAIL server
-$mail->Username      = "envios@alcaide.info"; // SMTP account username
-$mail->Password      = "malad_2122AASS";        // SMTP account password
+$mail->Username      = "xxxxxx"; // SMTP account username
+$mail->Password      = "xxxxxx";        // SMTP account password
 $mail->SetFrom("envios@alcaide.info");
 $mail->AddReplyTo("envios@alcaide.info");
 $mail->CharSet="UTF-8";    // FORZAR UTFO EN EL EMAIL
