7d1fdb41d78749ffc97312a705f8e0d61baaa351
Angel <alqaid@gmail.com>
2020-12-12 09:29:52 +0100

    aviso legal

--- vista/pie.php
+++ vista/pie.php
@@ -7,8 +7,11 @@
 	<a class="navbar-brand " href="descripcion.php" data-toggle="tooltip" title="Para que sirve esta web?!">Que hacemos</a>
     <a class="navbar-brand " href="Conocenos.php" data-toggle="tooltip" title="Conocenos!">Quienes somos</a>
     <a class="navbar-brand " href="Contacto.php" data-toggle="tooltip" title="Contactanos!">Contacto</a>
-</div>
+ 
+	
 </nav>
-
-
+ 
+<nav class="navbar navbar-expand-sm  navbar-dark bg-dark justify-content-center">	 
+       <a class="avisolegal"   href="../vista/avisolegal.php">Aviso legal | Política de privacidad | Política de cookies | Condiciones de contratación</a> 
+ </nav>
 <script type="text/javascript" src="../vista/js/pie.js"></script>	
